e4b346aa1739697b28ffe9df663d5e557b37458b
iradukunda1 <iradujdd4@gmail.com>
2024-04-30 02:51:18 +0200

    Refactor and change binary file name.

--- install.ps1
+++ install.ps1
@@ -1,18 +1,18 @@
-$GITHUB_REPOSITORY="quarksgroup/drop-cli"
+$GITHUB_REPOSITORY="quarksgroup/andasy-cli"
 
 $LATEST_VERSION=Invoke-WebRequest -Uri "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/latest" -UseBasicParsing | Select-Object -ExpandProperty Content | ConvertFrom-Json -ErrorAction Stop | Select-Object -ExpandProperty tag_name
 
-Invoke-WebRequest -Uri "https://github.com/$GITHUB_REPOSITORY/releases/download/$LATEST_VERSION/dropctl-windows-amd64.zip" -OutFile "dropctl.zip"
+Invoke-WebRequest -Uri "https://github.com/$GITHUB_REPOSITORY/releases/download/$LATEST_VERSION/andasy-windows-amd64.zip" -OutFile "andasy.zip"
 
-New-Item -ItemType Directory -Force -Path "$env:APPDATA\dropctl" | Out-Null
+New-Item -ItemType Directory -Force -Path "$env:APPDATA\andasy" | Out-Null
 		
-Expand-Archive -Force -Path "dropctl.zip" -DestinationPath "$env:APPDATA\dropctl"
+Expand-Archive -Force -Path "andasy.zip" -DestinationPath "$env:APPDATA\andasy"
 		
-Remove-Item -Path "dropctl.zip"
+Remove-Item -Path "andasy.zip"
 		
-if ($env:PATH -notlike "*$env:APPDATA\dropctl*") {
-  $env:PATH += ";$env:APPDATA\dropctl"
+if ($env:PATH -notlike "*$env:APPDATA\andasy*") {
+  $env:PATH += ";$env:APPDATA\andasy"
 	[Environment]::SetEnvironmentVariable("PATH", $env:PATH, [System.EnvironmentVariableTarget]::User)
 }
 
-Write-Host "dropctl installed successfully."
+Write-Host "andasy installed successfully."
